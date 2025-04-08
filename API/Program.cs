68eb14fec7ed2f1835d3ca0545e98df197a836e4
TuanLA53 <TuanLA53@gmail.com>
2025-04-08 19:49:24 +0700

    Add exception handling middleware and error response DTOs; implement product creation DTO

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -7,22 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<StoreContext>(opt =>{
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
 
 try
 {
     using var scope = app.Services.CreateScope();
     var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<StoreContext>(); 
+    var context = services.GetRequiredService<StoreContext>();
     await context.Database.MigrateAsync(); // This will create the database if it doesn't exist
     await StoreContextSeed.SeedAsyns(context); // This will seed the database with the products.json file
 }
